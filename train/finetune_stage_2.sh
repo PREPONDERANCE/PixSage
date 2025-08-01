73433607a16a2045378a4b2fae53f0b66bd006e3
JimGu <gwt200407@gmail.com>
2025-08-01 09:36:05 +0800

    feat: add second stage finetune

---
+++ train/finetune_stage_2.sh
@@ -0,0 +1,73 @@
+set -x
+
+GPUS=${GPUS:-2}
+BATCH_SIZE=${BATCH_SIZE:-64}
+PER_DEVICE_BATCH_SIZE=${PER_DEVICE_BATCH_SIZE:-4}
+GRADIENT_ACC=$((BATCH_SIZE / PER_DEVICE_BATCH_SIZE / GPUS))
+
+
+export PYTHONPATH="${PYTHONPATH}:$(pwd)"
+export MASTER_PORT=34229
+export TF_CPP_MIN_LOG_LEVEL=3
+export LAUNCHER=pytorch
+
+OUTPUT_DIR='output'
+
+if [ ! -d "$OUTPUT_DIR" ]; then
+  mkdir -p "$OUTPUT_DIR"
+fi
+
+# number of gpus: 8
+# batch size per gpu: 4
+# gradient accumulation steps: 4
+# total batch size: 64
+# epoch: 1
+torchrun \
+  --nnodes=1 \
+  --node_rank=0 \
+  --master_addr=127.0.0.1 \
+  --nproc_per_node=${GPUS} \
+  --master_port=${MASTER_PORT} \
+  internvl/train/internvl_chat_finetune.py \
+  --model_name_or_path "output/checkpoint_2200" \
+  --conv_style "internvl2_5" \
+  --use_fast_tokenizer False \
+  --output_dir ${OUTPUT_DIR} \
+  --meta_path "dataset/meta.json" \
+  --use_llm_lora 8 \
+  --use_backbone_lora 8 \
+  --overwrite_output_dir True \
+  --force_image_size 448 \
+  --max_dynamic_patch 12 \
+  --down_sample_ratio 0.5 \
+  --drop_path_rate 0.0 \
+  --freeze_llm True \
+  --freeze_mlp False \
+  --freeze_backbone True \
+  --vision_select_layer -1 \
+  --dataloader_num_workers 4 \
+  --bf16 True \
+  --num_train_epochs 50 \
+  --per_device_train_batch_size ${PER_DEVICE_BATCH_SIZE} \
+  --gradient_accumulation_steps ${GRADIENT_ACC} \
+  --evaluation_strategy "steps" \
+  --eval_steps 0.25 \
+  --per_device_eval_batch_size ${PER_DEVICE_BATCH_SIZE} \
+  --save_strategy "steps" \
+  --save_steps 200 \
+  --save_total_limit 1 \
+  --learning_rate 8e-6 \
+  --weight_decay 0.05 \
+  --warmup_ratio 0.03 \
+  --lr_scheduler_type "cosine" \
+  --logging_steps 1 \
+  --max_seq_length 16384 \
+  --do_train True \
+  --grad_checkpoint True \
+  --group_by_length True \
+  --dynamic_image_size True \
+  --use_thumbnail True \
+  --ps_version 'v2' \
+  --deepspeed "train/zero_stage1_config.json" \
+  --report_to "tensorboard" \
+  2>&1 | tee -a "${OUTPUT_DIR}/training_log.txt"
