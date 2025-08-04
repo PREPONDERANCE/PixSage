3819397999202352443d5d7d0f99edd59b9aa4fb
JimGu <gwt200407@gmail.com>
2025-08-04 21:42:50 +0800

    feat: merge weights after training

---
+++ internvl/tools/merge_weights.py
@@ -0,0 +1,34 @@
+import torch
+
+from pathlib import Path
+from transformers import AutoTokenizer
+
+from config import settings
+from internvl.model.internvl_chat import InternVLChatModel
+
+
+def merge(model_path: str, dest_path: str):
+    print("Loading model...")
+    model = InternVLChatModel.from_pretrained(
+        model_path, low_cpu_mem_usage=True, torch_dtype=torch.bfloat16
+    ).eval()
+    print("Loading tokenizer...")
+    tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
+
+    lora_weight = torch.load(Path(model_path) / settings.LORA_WEIGHT, weights_only=True)
+    model.load_state_dict(lora_weight, strict=False)
+
+    if model.config.use_backbone_lora:
+        model.vision_model.merge_and_unload()
+        model.vision_model = model.vision_model.model
+        model.config.use_backbone_lora = 0
+    if model.config.use_llm_lora:
+        model.language_model.merge_and_unload()
+        model.language_model = model.language_model.model
+        model.config.use_llm_lora = 0
+
+    print("Saving model...")
+    model.save_pretrained(dest_path)
+    print("Saving tokenizer...")
+    tokenizer.save_pretrained(dest_path)
+    print("Done!")
