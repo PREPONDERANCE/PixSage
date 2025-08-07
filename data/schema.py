99f08eb9df396b339b151b037f2aad3047fb8c97
JimGu <gwt200407@gmail.com>
2025-08-07 12:00:31 +0800

    fix: fix py39 incompatible type hints

--- data/schema.py
+++ data/schema.py
@@ -1,4 +1,4 @@
-from typing import Dict, List, Any
+from typing import Dict, List, Any, Union
 
 from pydantic import BaseModel, Field
 
@@ -22,7 +22,7 @@ class AnnotationInternVL(BaseModel):
     image: str
     width: int
     height: int
-    score: int | float = 0
+    score: Union[int, float] = 0
     metric: str = ""
     conversations: List[ChatInternVL]
 
